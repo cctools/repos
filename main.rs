1745592fb9ecde9e53945bf4e67b58fe5b44be4c
Wilhelm Hugo Kirschbaum <wkirschbaum@gmail.com>
2020-09-14 07:42:08 +0200

    Make http call
    I am just exploring rust, so this has no real meaning but to learn

--- main.rs
+++ main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, World!");
+use hyper::{Client, Uri};
+
+#[tokio::main]
+async fn main() {
+    let client = Client::new();
+
+    let url: Uri = "http://httpbin.org/response-headers?foo=bar"
+        .parse()
+        .unwrap();
+
+    match client.get(url).await {
+        Ok(res) => println!("Response: {}", res.status()),
+        Err(err) => println!("Error: {}", err),
+    }
 }
